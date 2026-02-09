de18f18a62d23d894a50b8700c3c52aeafe4cc00
Raka <maharjanraka2015@gmail.com>
2026-02-09 18:47:45 +0545

    some misc

---
+++ 01-Basics/hello.py
@@ -0,0 +1,14 @@
+# greetings = 'Hello World !'
+# print(greetings)
+
+meaning = 8
+print('')
+
+# if meaning > 10:
+#     print('Right on !')
+# else:
+#     print('Not Today')
+
+#Ternay Operator
+print('Right On') if meaning > 10 else print('Not Today')
+
