e998f350e313105d47d3502825473983498b0cc5
Raka <maharjanraka2015@gmail.com>
2026-02-10 19:30:08 +0545

    dictionaries has been added.

--- 04-Dictionaries/dictionaries.py
+++ 04-Dictionaries/dictionaries.py
@@ -6,145 +6,146 @@ band = {
 
 band2 = dict(vocals="Plant", guitar="Page")
 
-print(band)
-print(band2)
-print(type(band))
-print(len(band))
+# print(band)
+# print(band2)
+# print(type(band))
+# print(len(band))
 
 # Access items
-print(band["vocals"])
-print(band.get("guitar"))
+# print(band["vocals"])
+# print(band.get("vocals"))
 
 # list all keys
-print(band.keys())
+# print(band.keys())
 
-# list all values
-print(band.values())
+# # list all values
+# print(band.values())
 
-# list of key/value pairs as tuples
-print(band.items())
+# # list of key/value pairs as tuples
+# print(band.items())
 
-# verify a key exists
-print("guitar" in band)
-print("triangle" in band)
+# # verify a key exists
+# print("guitar" in band)
+# print("triangle" in band)
 
-# Change values
-band["vocals"] = "Coverdale"
-band.update({"bass": "JPJ"})
+# # Change values
+# band["vocals"] = "Coverdale"
+# print(band['vocals'])
+# band.update({"bass": "JPJ"})
+# print(band)
 
-print(band)
 
-# Remove items
-print(band.pop("bass"))
-print(band)
+# # Remove items
+# print(band.pop("bass"))
+# print(band)
 
-band["drums"] = "Bonham"
-print(band)
+# band["drums"] = "Bonham"
+# print(band)
 
-print(band.popitem())  # tuple
-print(band)
+# print(band.popitem())  # tuple
+# print(band)
 
-# Delete and clear
+# # Delete and clear
 
-band["drums"] = "Bonham"
-del band["drums"]
-print(band)
+# band["drums"] = "Bonham"
+# del band["drums"]
+# print(band)
 
-band2.clear()
-print(band2)
-
-del band2
-
-# Copy dictionaries
-
-# band2 = band  # create a reference
-# print("Bad copy!")
+# band2.clear()
 # print(band2)
-# print(band)
 
+# del band2
+
+# # Copy dictionaries
+
+# # band2 = band  # create a reference
+# # print("Bad copy!")
+# # print(band2)
+# # print(band)
+
+# # band2["drums"] = "Dave"
+# # print(band)
+
+# band2 = band.copy()
 # band2["drums"] = "Dave"
+# print("Good copy!")
 # print(band)
+# print(band2)
 
-band2 = band.copy()
-band2["drums"] = "Dave"
-print("Good copy!")
-print(band)
-print(band2)
+# # or use the dict() constructor function
+# band3 = dict(band)
+# print("Good copy!")
+# print(band3)
 
-# or use the dict() constructor function
-band3 = dict(band)
-print("Good copy!")
-print(band3)
+# # Nested dictionaries
 
-# Nested dictionaries
+# member1 = {
+#     "name": "Plant",
+#     "instrument": "vocals"
+# }
+# member2 = {
+#     "name": "Page",
+#     "instrument": "guitar"
+# }
+# band = {
+#     "member1": member1,
+#     "member2": member2
+# }
+# print(band)
+# print(band["member1"]["name"])
 
-member1 = {
-    "name": "Plant",
-    "instrument": "vocals"
-}
-member2 = {
-    "name": "Page",
-    "instrument": "guitar"
-}
-band = {
-    "member1": member1,
-    "member2": member2
-}
-print(band)
-print(band["member1"]["name"])
+# # Sets
 
-# Sets
+# nums = {1, 2, 3, 4}
 
-nums = {1, 2, 3, 4}
+# nums2 = set((1, 2, 3, 4))
 
-nums2 = set((1, 2, 3, 4))
+# print(nums)
+# print(nums2)
+# print(type(nums))
+# print(len(nums))
 
-print(nums)
-print(nums2)
-print(type(nums))
-print(len(nums))
+# # No duplicate allowed
+# nums = {1, 2, 2, 3}
+# print(nums)
 
-# No duplicate allowed
-nums = {1, 2, 2, 3}
-print(nums)
+# # True is a dupe of 1, False is a dupe of zero
+# nums = {1, True, 2, False, 3, 4, 0}
+# print(nums)
 
-# True is a dupe of 1, False is a dupe of zero
-nums = {1, True, 2, False, 3, 4, 0}
-print(nums)
+# # check if a value is in a set
+# print(2 in nums)
 
-# check if a value is in a set
-print(2 in nums)
+# # but you cannot refer to an element in the set with an index position or a key
 
-# but you cannot refer to an element in the set with an index position or a key
+# # Add a new element to a set
+# nums.add(8)
+# print(nums)
 
-# Add a new element to a set
-nums.add(8)
-print(nums)
+# # Add elements from one set to another
+# morenums = {5, 6, 7}
+# nums.update(morenums)
+# print(nums)
 
-# Add elements from one set to another
-morenums = {5, 6, 7}
-nums.update(morenums)
-print(nums)
+# # you can use update with lists, tuples, and dictionaries, too.
 
-# you can use update with lists, tuples, and dictionaries, too.
+# # Merge two sets to create a new set
+# one = {1, 2, 3}
+# two = {5, 6, 7}
 
-# Merge two sets to create a new set
-one = {1, 2, 3}
-two = {5, 6, 7}
+# mynewset = one.union(two)
+# print(mynewset)
 
-mynewset = one.union(two)
-print(mynewset)
+# # Keep only the duplicates
+# one = {1, 2, 3}
+# two = {2, 3, 4}
 
-# Keep only the duplicates
-one = {1, 2, 3}
-two = {2, 3, 4}
+# one.intersection_update(two)
+# print(one)
 
-one.intersection_update(two)
-print(one)
+# # Keep everything except the duplicates
+# one = {1, 2, 3}
+# two = {2, 3, 4}
 
-# Keep everything except the duplicates
-one = {1, 2, 3}
-two = {2, 3, 4}
-
-one.symmetric_difference_update(two)
-print(one)
+# one.symmetric_difference_update(two)
+# print(one)
