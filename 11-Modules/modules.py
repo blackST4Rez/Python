0c6d48746674790d17d275bc6d32d1cc69a82181
Raka <maharjanraka2015@gmail.com>
2026-02-15 16:14:02 +0545

    modules has been added.

---
+++ 11-Modules/modules.py
@@ -0,0 +1,21 @@
+
+
+from math import pi
+import sys
+import random as rdm
+from enum import Enum
+import kathmandu
+from rps import rock_paper_scissors
+
+print(pi)
+
+# for item in dir(rdm):
+#     print(item)
+
+print(kathmandu.capital)
+kathmandu.randomfunfact()
+
+print(__name__)
+print(kathmandu.__name__)
+
+rock_paper_scissors()
